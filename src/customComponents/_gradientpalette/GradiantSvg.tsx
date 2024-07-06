d90d9254a20761789d5a0a51897749a5920fbeba
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2024-07-06 17:13:44 +0530

    test

--- src/customComponents/_gradientpalette/GradiantSvg.tsx
+++ src/customComponents/_gradientpalette/GradiantSvg.tsx
@@ -121,9 +121,9 @@ function GradiantSvg({
           x2="296.377"
           y2="348.833"
           gradientUnits="userSpaceOnUse">
-          <stop offset="0.367" stop-color="#20C9FF" />
-          <stop offset="0.844" stop-color="#0008BB" />
-          <stop offset="1" stop-color="white" />
+          <stop offset="0.367" stopColor="#20C9FF" />
+          <stop offset="0.844" stopColor="#0008BB" />
+          <stop offset="1" stopColor="white" />
         </linearGradient>
         <radialGradient
           id="paint1_radial_16_2"
@@ -132,9 +132,9 @@ function GradiantSvg({
           r="1"
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(697 31) rotate(43.5077) scale(352.966 352.284)">
-          <stop offset="0.188406" stop-color="#F7EAA3" />
-          <stop offset="0.38704" stop-color="#ECB4EE" stop-opacity="0.56" />
-          <stop offset="1" stop-color="#A3CBF7" />
+          <stop offset="0.188406" stopColor="#F7EAA3" />
+          <stop offset="0.38704" stopColor="#ECB4EE" stopOpacity="0.56" />
+          <stop offset="1" stopColor="#A3CBF7" />
         </radialGradient>
         <radialGradient
           id="paint2_radial_16_2"
@@ -143,8 +143,8 @@ function GradiantSvg({
           r="1"
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(515.5 169) rotate(43.2189) scale(216.126 215.708)">
-          <stop stop-color="#FF5A08" />
-          <stop offset="1" stop-color="#580000" />
+          <stop stopColor="#FF5A08" />
+          <stop offset="1" stopColor="#580000" />
         </radialGradient>
         <linearGradient
           id="paint3_linear_16_2"
@@ -153,8 +153,8 @@ function GradiantSvg({
           x2="1356.39"
           y2="26.9514"
           gradientUnits="userSpaceOnUse">
-          <stop offset="0.052" stop-color="#489A4E" />
-          <stop offset="0.959" stop-color="#FBCE46" />
+          <stop offset="0.052" stopColor="#489A4E" />
+          <stop offset="0.959" stopColor="#FBCE46" />
         </linearGradient>
         <radialGradient
           id="paint4_radial_16_2"
@@ -163,8 +163,8 @@ function GradiantSvg({
           r="1"
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(24.5 346.5) rotate(43.1862) scale(424.477 423.657)">
-          <stop offset="0.178" stop-color="#FF5EF7" />
-          <stop offset="0.798803" stop-color="#02F5FF" />
+          <stop offset="0.178" stopColor="#FF5EF7" />
+          <stop offset="0.798803" stopColor="#02F5FF" />
         </radialGradient>
         <linearGradient
           id="paint5_linear_16_2"
@@ -173,10 +173,10 @@ function GradiantSvg({
           x2="1068.68"
           y2="460.067"
           gradientUnits="userSpaceOnUse">
-          <stop offset="0.055" stop-color="#55E4E0" />
-          <stop offset="0.542" stop-color="#4B44E0" stop-opacity="0.74" />
-          <stop offset="0.552" stop-color="#40C6EE" />
-          <stop offset="0.984" stop-color="#B124E0" />
+          <stop offset="0.055" stopColor="#55E4E0" />
+          <stop offset="0.542" stopColor="#4B44E0" stopOpacity="0.74" />
+          <stop offset="0.552" stopColor="#40C6EE" />
+          <stop offset="0.984" stopColor="#B124E0" />
         </linearGradient>
         <linearGradient
           id="paint6_linear_16_2"
@@ -185,8 +185,8 @@ function GradiantSvg({
           x2="676.259"
           y2="633.453"
           gradientUnits="userSpaceOnUse">
-          <stop offset="0.008" stop-color="#C20027" />
-          <stop offset="0.994" stop-color="#0A2368" />
+          <stop offset="0.008" stopColor="#C20027" />
+          <stop offset="0.994" stopColor="#0A2368" />
         </linearGradient>
         <linearGradient
           id="paint7_linear_16_2"
@@ -195,12 +195,12 @@ function GradiantSvg({
           x2="1352.1"
           y2="635.826"
           gradientUnits="userSpaceOnUse">
-          <stop offset="0.003" stop-color="#CC9CDF" />
-          <stop offset="0.204" stop-color="#9BDBF5" />
-          <stop offset="0.402" stop-color="#F5B7D0" />
-          <stop offset="0.629" stop-color="#FABA62" />
-          <stop offset="0.819" stop-color="#EAEEF0" />
-          <stop offset="0.992" stop-color="#FAF5AB" />
+          <stop offset="0.003" stopColor="#CC9CDF" />
+          <stop offset="0.204" stopColor="#9BDBF5" />
+          <stop offset="0.402" stopColor="#F5B7D0" />
+          <stop offset="0.629" stopColor="#FABA62" />
+          <stop offset="0.819" stopColor="#EAEEF0" />
+          <stop offset="0.992" stopColor="#FAF5AB" />
         </linearGradient>
         <linearGradient
           id="paint8_linear_16_2"
@@ -209,9 +209,9 @@ function GradiantSvg({
           x2="392.529"
           y2="802.545"
           gradientUnits="userSpaceOnUse">
-          <stop offset="0.112" stop-color="#FF1886" />
-          <stop offset="0.52" stop-color="#FCE844" />
-          <stop offset="1" stop-color="#35B2EF" />
+          <stop offset="0.112" stopColor="#FF1886" />
+          <stop offset="0.52" stopColor="#FCE844" />
+          <stop offset="1" stopColor="#35B2EF" />
         </linearGradient>
         <radialGradient
           id="paint9_radial_16_2"
@@ -220,8 +220,8 @@ function GradiantSvg({
           r="1"
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(765 729.5) rotate(42.6467) scale(335.805 335.157)">
-          <stop stop-color="#12233C" />
-          <stop offset="0.731481" stop-color="#BBBBBB" />
+          <stop stopColor="#12233C" />
+          <stop offset="0.731481" stopColor="#BBBBBB" />
         </radialGradient>
         <radialGradient
           id="paint10_radial_16_2"
@@ -230,8 +230,8 @@ function GradiantSvg({
           r="1"
           gradientUnits="userSpaceOnUse"
           gradientTransform="translate(515.5 809) rotate(43.2189) scale(216.126 215.708)">
-          <stop stop-color="#FDCB32" />
-          <stop offset="1" stop-color="#F43862" />
+          <stop stopColor="#FDCB32" />
+          <stop offset="1" stopColor="#F43862" />
         </radialGradient>
         <linearGradient
           id="paint11_linear_16_2"
@@ -240,9 +240,9 @@ function GradiantSvg({
           x2="1352.64"
           y2="662.764"
           gradientUnits="userSpaceOnUse">
-          <stop offset="0.03" stop-color="white" />
-          <stop offset="0.346" stop-color="#EB9E62" />
-          <stop offset="0.637" stop-color="#B10A0A" />
+          <stop offset="0.03" stopColor="white" />
+          <stop offset="0.346" stopColor="#EB9E62" />
+          <stop offset="0.637" stopColor="#B10A0A" />
           <stop offset="1" />
         </linearGradient>
       </defs>
--- src/customComponents/blogs/BlogCard.tsx
+++ src/customComponents/blogs/BlogCard.tsx
@@ -3,6 +3,7 @@ import { BlogsTypes } from "./blogsTypes";
 import { months } from "empyreanui/utils";
 import Link from "next/link";
 import Image from "next/image";
+import { Button } from "empyreanui/components/ui/button";
 
 function BlogCard({ key, data }: { key: any; data: BlogsTypes }) {
   const date = new Date(data.date).toLocaleDateString();
@@ -10,31 +11,41 @@ function BlogCard({ key, data }: { key: any; data: BlogsTypes }) {
     months[parseInt(date.split("/")[1], 10)] + " / " + date.split("/")[2];
 
   return (
-    <Link href={`/viewblog/${data._id}`}>
-      <div
-        key={key}
-        className="shadow-cardShadow rounded-3xl p-6 h-full hover:scale-105 transition-transform duration-200 ease-out w-full max-w-md mx-auto bg-white cursor-pointer">
-        <h1 className="text-2xl lg:text-3xl font-bold capitalize mb-4 text-gray-800">
-          {data.title}
-        </h1>
-        {data.thumbnail ? (
+    <div
+      key={key}
+      className="group h-full shadow-lg rounded-xl overflow-hidden hover:shadow-xl transition-shadow duration-300 ease-in-out w-full max-w-md mx-auto bg-white cursor-pointer flex flex-col">
+      {data.thumbnail ? (
+        <div className="relative w-full h-48">
           <Image
             src={data.thumbnail}
-            alt={data.thumbnail}
-            height={1000}
-            width={1000}
-            className="w-full h-40 mb-3"
+            alt={data.title}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-t-xl transition-transform duration-300 group-hover:scale-105"
           />
-        ) : null}
-        <h2 className="mb-4 text-gray-600">
-          <span className="font-semibold capitalize">{data.author}</span>{" "}
-          <span className="text-sm text-gray-500">on {formattedDate}</span>
-        </h2>
-        <p className="line-clamp-4 text-gray-700 leading-relaxed">
-          {data.content}
-        </p>
+        </div>
+      ) : null}
+      <div className="p-6 flex flex-col flex-grow">
+        <div className="flex-grow">
+          <h1 className="text-2xl font-bold capitalize mb-2 text-gray-800">
+            {data.title}
+          </h1>
+          <h2 className="text-sm text-gray-600 mb-4">
+            {data.author} on {formattedDate}
+          </h2>
+          <p className="line-clamp-3 text-gray-700 leading-relaxed text-sm mb-4">
+            {data.content}
+          </p>
+        </div>
+        <div className="flex justify-end mt-4">
+          <Link href={`/viewblog/${data._id}`}>
+            <Button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors duration-200">
+              Read More
+            </Button>
+          </Link>
+        </div>
       </div>
-    </Link>
+    </div>
   );
 }
 

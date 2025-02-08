1a7e4e8108cab9ab8fe94227d825c6290f32a5d8
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2025-02-09 01:19:03 +0530

    updated seo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,26 +3,10 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: [
-        "Googlebot",
-        "Bingbot",
-        "Googlebot-Image",
-        "Googlebot-News",
-        "Googlebot-Video",
-        "msnbot-media", // Bing's image bot
-        "DuckDuckBot",
-        "Slurp",
-        "Baiduspider",
-        "Yandex",
-        "Sogou web spider",
-        "Applebot",
-        "Facebot",
-        "LinkedInBot",
-        "Pinterest",
-      ],
-      allow: ["/", "/api/og/*"],
+      userAgent: "*",
+      allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://empyreanui-core.vercel.app//sitemap.xml",
+    sitemap: "https://empyreanui-core.vercel.app/sitemap.xml",
   };
 }
